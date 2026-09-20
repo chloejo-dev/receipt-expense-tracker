67ddc642d6c02ab0babae24ddd0afd6df27a8ef5
Chloe Jo <119808693+chloejo-dev@users.noreply.github.com>
2026-09-20 14:30:40 -0300

    feat: add receipt record list and detail view UI (#27)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,9 @@ import { Routes, Route } from "react-router-dom";
 import SignInPage from "./pages/SignInPage";
 import SignUpPage from "./pages/SignUpPage";
 import DashBoard from "./pages/Dashboard";
-import AddExpensePage from "./pages/AddExpensePage";
+import AddReceiptPage from "./pages/AddReceiptPage";
+import ExpenseHistoryPage from "./pages/ExpenseHistoryPage";
+import ReceiptDetailsPage from "./pages/ReceiptDetailsPage";
 
 function App() {
   return (
@@ -10,7 +12,9 @@ function App() {
       <Route path='/sign-up' element={<SignUpPage />} />
       <Route path='/' element={<SignInPage />} />
       <Route path='/dashboard' element={<DashBoard />} />
-      <Route path='/add-expense' element={<AddExpensePage />} />
+      <Route path='/add-receipt' element={<AddReceiptPage />} />
+      <Route path='/receipt-details' element={<ReceiptDetailsPage />} />
+      <Route path='/expense-history' element={<ExpenseHistoryPage />} />
     </Routes>
   );
 }
