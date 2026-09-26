ef7d83d9283cc6d76b69a91b73bac7eec609ca90
Chloe Jo <hyeyoonjo321@gmail.com>
2026-09-26 10:01:40 -0300

    feat: display expense history from API

--- frontend/src/pages/ExpenseHistoryPage.tsx
+++ frontend/src/pages/ExpenseHistoryPage.tsx
@@ -1,92 +1,131 @@
-import { Coffee, Bus, Hospital, Utensils, ShoppingCart } from "lucide-react";
+import {
+  Coffee,
+  ShoppingCart,
+  Bus,
+  Film,
+  Hospital,
+  Utensils,
+  Grid2X2,
+} from "lucide-react";
 import "./ExpenseHistoryPage.css";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-type Expense = {
-  id: number;
+type Receipt = {
+  receiptId: number;
   date: string;
-  store: string;
-  category: string;
-  amount: number;
+  storeName: string;
+  categoryLabel: string;
+  totalAmount: number;
 };
 
-// type ExpensesByDate = {
-//   [date: string]: Expense[];
-// };
+const CategoryLabels = {
+  Dining: {
+    icon: <Coffee />,
+    color: "#EF4444",
+    backgroundColor: "#FEE2E2",
+  },
+  Groceries: {
+    icon: <ShoppingCart />,
+    color: "#22C55E",
+    backgroundColor: "#DCFCE7",
+  },
+  Transportation: {
+    icon: <Bus />,
+    color: "#3B82F6",
+    backgroundColor: "#DBEAFE",
+  },
+  Entertainment: {
+    icon: <Film />,
+    color: "#A855F7",
+    backgroundColor: "#F3E8FF",
+  },
+  "Multiple Categories": {
+    icon: <Grid2X2 />,
+    color: "#F59E08",
+    backgroundColor: "#FEF3C7",
+  },
+};
 
 export default function ExpenseHistoryPage() {
-  //   const [expenses, setExpenses] = useState([]);
-  // Hard-coded data to be replaced with actual data in DB
-  const expenses = [
-    {
-      id: 1,
-      date: "Aug 12",
-      store: "Walmart",
-      category: "Groceries",
-      amount: 37.83,
-    },
-    {
-      id: 2,
-      date: "Aug 13",
-      store: "Starbucks",
-      category: "Dining",
-      amount: 12.89,
-    },
-    {
-      id: 3,
-      date: "Aug 13",
-      store: "Best Buy",
-      category: "Electronics",
-      amount: 105.83,
-    },
-    {
-      id: 4,
-      date: "Aug 14",
-      store: "Sobeys",
-      category: "Groceries",
-      amount: 22.53,
-    },
-  ];
+  const [receipts, setReceipts] = useState<Receipt[]>([]);
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const getReceipts = async () => {
+      setError("");
+
+      // Call API to get the user's receipts
+      try {
+        const res = await fetch(
+          `${import.meta.env.VITE_BASE_URL}/api/receipts`,
+          {
+            // Tell the browser to include cookie (credentials)
+            credentials: "include",
+          },
+        );
+
+        // 401 response -> Redirect to the sign in page
+        if (res.status === 401) {
+          navigate("/", { replace: true });
+          return;
+        }
+
+        // Handle other failure responses
+        if (!res.ok) {
+          setError(
+            "Failed to retrieve expense history. Please try again later.",
+          );
+          setIsLoading(false);
+          return;
+        }
+
+        const data = await res.json();
+        setReceipts(data);
+        setIsLoading(false);
+      } catch {
+        setError("Failed to retrieve expense history. Please try again later.");
+        setIsLoading(false);
+      }
+    };
+
+    getReceipts();
+  }, [navigate]);
 
   // Group expenses by date
-  // const expensesByDate = expenses.reduce(
-  //   (expensesByDate: ExpensesByDate, expense) => {
-  //     if (expense.date in expensesByDate) {
-  //       expensesByDate[expense.date].push(expense);
-  //     } else {
-  //       expensesByDate[expense.date] = [expense];
-  //     }
-  //     return expensesByDate;
-  //   },
-  //   {},
-  // );
-
-  // const expensesByDateArr = Object.entries(expensesByDate);
-
-  // console.log(expensesByDateArr);
+  const receiptsByDate = Object.groupBy(receipts, (receipt) => receipt.date);
+  const receiptGroups = Object.entries(receiptsByDate); // [[key, value], [key, value], ...]
 
   return (
-    <div className='expense-history'>
-      <header className='expense-header'>
+    <div className='history-page'>
+      <header className='history-header'>
         <h1>Expense History</h1>
       </header>
       <main className='history-content'>
-        <section className='expense-date-group'>
-          <h2 className='expense-date'>Today, Oct 24</h2>
-          {expenses.map((expense) => (
-            <article key={expense.id} className='expense-card'>
-              <div className='expense-icon'>
-                <Coffee />
-              </div>
-              <div className='history-details'>
-                <h3 className='expense-store'>{expense.store}</h3>
-                <p className='expense-category'>{expense.category}</p>
-              </div>
-              <strong className='expense-amount'>
-                ${expense.amount.toFixed(2)}
-              </strong>
-            </article>
-          ))}
-        </section>
+        {isLoading ? (
+          <p>Loading receipts...</p>
+        ) : error ? (
+          <p>{error}</p>
+        ) : receiptGroups.length === 0 ? (
+          <p>You have no expense history yet. Ready to add one?</p>
+        ) : (
+          receiptGroups.map(([date, dailyReceipts]) => (
+            <section className='receipt-group' key={date}>
+              <h2 className='receipt-date'>{date}</h2>
+              {dailyReceipts?.map((receipt) => (
+                <article key={receipt.receiptId} className='receipt-card'>
+                  <Coffee />
+                  <h3>{receipt.storeName}</h3>
+                  <p className='receipt-total-amount'>${receipt.totalAmount}</p>
+                  <p>{receipt.categoryLabel}</p>
+                </article>
+              ))}
+            </section>
+          ))
+        )}
       </main>
     </div>
   );
